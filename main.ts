9b0e9fa640ed3dea35d9ee398df5a646da91a088
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-20 11:10:04 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,46 @@
-basic.forever(function () {
-	
+input.onGesture(Gesture.Shake, function () {
+    PickShape()
 })
+function PickShape () {
+    my_hand = randint(1, 3)
+    if (my_hand == rock_index) {
+        Rock_image.showImage(0)
+    }
+    if (my_hand == paper_index) {
+        Paper_image.showImage(0)
+    }
+    if (my_hand == scissors_index) {
+        Scissors_image.showImage(0)
+    }
+}
+let my_hand = 0
+let Scissors_image: Image = null
+let Paper_image: Image = null
+let Rock_image: Image = null
+let scissors_index = 0
+let paper_index = 0
+let rock_index = 0
+rock_index = 1
+paper_index = 2
+scissors_index = 3
+Rock_image = images.createImage(`
+    . . . . .
+    . # # # .
+    # # # # #
+    # # # # #
+    . . . . .
+    `)
+Paper_image = images.createImage(`
+    # # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+Scissors_image = images.createImage(`
+    # # . . #
+    # # . # .
+    . . # . .
+    # # . # .
+    # # . . #
+    `)

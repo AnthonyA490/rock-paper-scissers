39bcdb88c8bd866931ade6cf20ff961fe5ed076a
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-20 20:00:58 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    num_ += 1
+    basic.showNumber(num_)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(num_)
+})
+input.onButtonPressed(Button.B, function () {
+    num_ += -1
+    basic.showNumber(num_)
+})
 input.onGesture(Gesture.Shake, function () {
     PickShape()
 })
@@ -14,12 +25,14 @@ function PickShape () {
     }
 }
 let my_hand = 0
+let num_ = 0
 let Scissors_image: Image = null
 let Paper_image: Image = null
 let Rock_image: Image = null
 let scissors_index = 0
 let paper_index = 0
 let rock_index = 0
+basic.showIcon(IconNames.Angry)
 rock_index = 1
 paper_index = 2
 scissors_index = 3
